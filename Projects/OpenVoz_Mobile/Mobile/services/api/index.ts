a01c83723bd26cd3e46b23b155e032f1df368b72
WaukyJose <joselema64@hotmail.com>
2026-08-21 08:22:54 -0500

    Export mobile data deletion request API

--- Projects/OpenVoz_Mobile/Mobile/services/api/index.ts
+++ Projects/OpenVoz_Mobile/Mobile/services/api/index.ts
@@ -1,6 +1,7 @@
 export * from './api-client';
 export * from './assessment-api';
 export * from './auth-api';
+export * from './data-deletion-request-api';
 export * from './dashboard-api';
 export * from './profile-api';
 export * from './speaking-api';
